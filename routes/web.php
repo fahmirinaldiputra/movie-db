241757275e45c8b02e9fa7ea6d81ba7209e9b2f4
fahmirinaldi <fahmirinaldi3011@gmail.com>
2025-06-02 15:46:16 +0700

    login dan logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,12 @@ Route::get('/', [MovieController::class, 'homepage']);
 
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.detail');
 
-Route::get('/create-movie', [MovieController::class, 'create']);
+Route::get('/create-movie', [MovieController::class, 'create'])->middleware('auth');
 
-Route::post('/create-movie', [MovieController::class, 'store']);
+Route::post('/create-movie', [MovieController::class, 'store'])->middleware('auth');
+
+Route::get('/login',[AuthController::class, 'loginForm'])->name('login')->middleware('guest');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout']);
